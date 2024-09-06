628c4fea9ff5f33c614ea45fa04acc3188796eef
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-09-06 13:59:12 +0200

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import Link from "./components/Link.tsx";
-import background from "./assets/background.jpg";
-import Title from "./components/Title.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HubPage from "./components/HubPage";
 
 function App() {
-
-
     return (
-        <div className={"absolute bg-cover h-screen w-screen"} style={{ backgroundImage: `url(${background})`}}>
-            <Title></Title>
-            <div className={"flex flex-col items-center"}>
-                <Link website={"discord"} soMeHandle={"lolgameplay"}
-                      url={"https://www.google.com"}></Link>
-                <Link website={"instagram"} soMeHandle={"lolgameplay"}
-                      url={"https://www.google.com"}></Link>
-                <Link website={"x"} soMeHandle={"lolgameplay"}
-                      url={"https://www.google.com"}></Link>
-            </div>
-        </div>
-
-    )
+        <Router>
+            <Routes>
+                <Route path="/" element={<HubPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/HubPage.tsx
@@ -0,0 +1,19 @@
+import background from "../assets/background.jpg";
+import Title from "./Title.tsx";
+import Link from "./Link.tsx";
+
+export default function HubPage(){
+    return (
+        <div className={"absolute bg-cover h-screen w-screen"} style={{backgroundImage: `url(${background})`}}>
+            <Title></Title>
+            <div className={"flex flex-col items-center"}>
+                <Link website={"discord"} soMeHandle={"lolgameplay"}
+                      url={"https://www.google.com"}></Link>
+                <Link website={"instagram"} soMeHandle={"lolgameplay"}
+                      url={"https://www.google.com"}></Link>
+                <Link website={"x"} soMeHandle={"lolgameplay"}
+                      url={"https://www.google.com"}></Link>
+            </div>
+        </div>
+    )
+}
