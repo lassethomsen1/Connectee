6ad173e0593dfda2715cb6daaebb7b9ac9191426
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-09-07 00:10:55 +0200

    Gets user session now

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -3,22 +3,40 @@
 import { Sidebar } from "@/components/Sidebar";
 import { Header } from "@/components/Header";
 import { LinksTable } from "@/components/LinksTable";
-import { Button } from "@/components/ui/button";
+import {useEffect, useState} from "react";
+import {supabase} from "../../supabase.ts";
+import {useNavigate} from "react-router-dom";
+import CreateLinkInputForm from "../CreateLinkInputForm.tsx";
+import {Button} from "../ui/button.tsx";
 
 export default function DashboardPage() {
+    const [user, setUser] = useState<any>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getUserSession() {
+            const { data: {session}} = await supabase.auth.getSession();
+            setUser(session?.user ?? null);
+        }
+
+        getUserSession();
+    }, []);
+
+    if (!user) {
+        navigate("/login");
+    }
+
     return (
         <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-            <Sidebar />
+            <Sidebar user={user}/>
             <div className="flex flex-col">
-                <Header />
+                <Header user={user} />
                 <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
                     <div className="flex items-center">
                         <h1 className="font-semibold text-lg md:text-2xl">Links</h1>
-                        <Button className="ml-auto" size="sm">
-                            Create Link
-                        </Button>
+                        <CreateLinkInputForm user={user} />
                     </div>
-                    <LinksTable />
+                    <LinksTable user={user}/>
                 </main>
             </div>
         </div>
