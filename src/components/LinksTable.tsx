2028dcecb3acfa0c27dde08f5d685f337a39b6f4
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-09-07 00:11:42 +0200

    fetch user's links

--- src/components/LinksTable.tsx
+++ src/components/LinksTable.tsx
@@ -2,9 +2,33 @@
 //@ts-nocheck
 import { Table, TableHeader, TableRow, TableHead, TableBody } from "@/components/ui/table";
 import LinkTableRow from "./LinkTableRow.tsx";
+import {useEffect, useState} from "react";
+import {supabase} from "../supabase.ts";
+interface LinkTableProps {
+    user: any;
+}
 
+export function LinksTable({ user }: LinkTableProps) {
+    const [links, setLinks] = useState<any[]>([]);
 
-export function LinksTable() {
+    useEffect(() => {
+        async function fetchLinks() {
+            const { data, error } = await supabase
+                .from("url")
+                .select("*")
+                .eq("user_id", user.id);
+
+            if (error) {
+                console.error("Error fetching links:", error.message);
+                return;
+            }
+            setLinks(data);
+        }
+
+        if (user) {
+            fetchLinks();
+        }
+    }, [user]);
     return (
         <div className="border shadow-sm rounded-lg">
             <Table>
@@ -17,7 +41,15 @@ export function LinksTable() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <LinkTableRow imgUrl={"https://cdn-icons-png.flaticon.com/128/733/733547.png"} url={"facebook.com"} handle={"Lasse thomsen"}/>
+                    {links.length=== 0 ? <TableRow><td colSpan={4} className="text-center font-medium">No links found</td></TableRow> : null}
+                    {links.map((link) => (
+                        <LinkTableRow
+                            key={link.id}
+                            imgUrl={link.icon_url}
+                            url={link.url}
+                            handle={link.handle}
+                        />
+                    ))}
                     {/* Repeat for other rows */}
                 </TableBody>
             </Table>
