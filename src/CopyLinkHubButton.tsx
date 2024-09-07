2e49a48c899fc500825161849d1b05ffafa7b0fa
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-09-07 18:30:41 +0200

    Changed usage of env for redirection

--- src/CopyLinkHubButton.tsx
+++ src/CopyLinkHubButton.tsx
@@ -2,14 +2,14 @@ import {Button} from "./components/ui/button.tsx";
 //todo lav alerten om til en toast
 export default function CopyLinkHubButton({userid}: {userid: string}) {
     const baseurl = import.meta.env.VITE_BASE_URL;
-    const url = baseurl + "/hub/" + userid;
+    const url = `${baseurl}/hub/${userid}`;
     return (
         <Button
             className="ml-auto"
             size="sm"
             onClick={() => {
                 navigator.clipboard.writeText(url);
-                alert("Link copied to clipboard!")
+                alert("Link copied to clipboard " + url);
             }}
         >
             Copy link
