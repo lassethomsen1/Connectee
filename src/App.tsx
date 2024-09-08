a93ee1980d28b4c10d270f5ad30d58e9be3f39a3
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-09-08 14:03:47 +0200

    temp landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import ConnectPage from "./components/pages/ConnectPage.tsx";
 import DashboardPage from "./components/pages/DashboardPage.tsx";
 import LoginPage from "./components/pages/LoginPage.tsx";
 import SignUpPage from "./components/pages/SignUpPage.tsx";
+import LandingPage from "./components/pages/LandingPage.tsx";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<ConnectPage />} />
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
                 <Route path={"/login"} element={<LoginPage/>}/>
                 <Route path={"/signup"} element={<SignUpPage/>}/>
---
+++ src/components/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage(){
+    return (
+        <div>
+            <h1 className={"text-center text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500"}>
+                Under construction
+            </h1>
+        </div>
+    )
+}
