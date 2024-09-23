38c928387fd1a5c2dc7ddb56e559158a4c42c769
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-09-23 13:20:36 +0200

    added retroMoney theme

--- src/components/pages/ConnectPage.tsx
+++ src/components/pages/ConnectPage.tsx
@@ -73,36 +73,14 @@ export default function ConnectPage() {
             'base-300': '#b8ab00',
             'fontFamily': "ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace",
         },
+        retroMoney: {
+            'primary': '#E5D9B6',
+            'primary-content': '#A4BE7B',
+            'secondary': '#5F8D4E',
+            'secondary-content': '#3A5A40',
+            'accent': '#344E41'
+        }
     }
-    /*
-    'cyberpunk': {
-           'primary' : '#ff7094',
-           'primary-focus' : '#ff5784',
-           'primary-content' : '#000000',
-
-           'secondary' : '#75d1f0',
-           'secondary-focus' : '#5bbedc',
-           'secondary-content' : '#000000',
-
-           'accent' : '#c07eec',
-           'accent-focus' : '#ad55e7',
-           'accent-content' : '#000000',
-
-           'neutral' : '#3d3a00',
-           'neutral-focus' : '#090901',
-           'neutral-content' : '#ffee00',
-
-           'base-100' : '#ffee00',
-           'base-200' : '#d6c800',
-           'base-300' : '#b8ab00',
-           'base-content' : '#000000',
-
-           'info' : '#1c92f2',
-           'success' : '#009485',
-           'warning' : '#ff9900',
-           'error' : '#ff5724',*/
-
-
     return (
         <div
             className="relative flex flex-col items-center min-h-screen text-white"
